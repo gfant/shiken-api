b6854ea137f8f1227dfba16f0d74681925e874eb
iam-agf <agf0710@gmail.com>
2024-06-14 02:16:46 -0600

    Explanations to complement README

--- executor/handler/constants.go
+++ executor/handler/constants.go
@@ -1,7 +1,9 @@
 package handler
 
 const (
-	headPath         = "/Users/iam-agf/github/shiken_server"
+	// Change this folder to the output of pwd while being in the folder where you cloned the repository
+	headPath = "/Users/iam-agf/github/shiken_server"
+	// Don't edit under this line
 	problemsPath     = "problems"
 	tmpTestingFolder = "tmp"
 )
--- reader/handler/constants.go
+++ reader/handler/constants.go
@@ -1,6 +1,8 @@
 package handler
 
 const (
-	headPath       = "/Users/iam-agf/github/shiken_server"
+	// Change this folder to the output of pwd while being in the folder where you cloned the repository
+	headPath = "/Users/iam-agf/github/shiken_server"
+	// Don't touch this value
 	problemsFolder = "problems"
 )
