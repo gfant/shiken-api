82ac83afba0fed5780e618871fb30bc578e0d30f
iam-agf <agf0710@gmail.com>
2024-06-26 01:45:52 -0600

    fixing CORS issue

--- executor/main.go
+++ executor/main.go
@@ -22,7 +22,7 @@ func setupCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		// Handle preflight request
