7e8a36d7d0121df767052daa8d8bc8c87dfb91c1
gfant <61362029+gfant@users.noreply.github.com>
2024-07-31 01:01:01 -0600

    reader not needed anymore since it's called from blockchain

--- reader/go.mod
+++
@@ -1,3 +0,0 @@
-module reader
-
-go 1.22.2
--- reader/handler/constants.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-const (
-	// Change this folder to the output of pwd while being in the folder where you cloned the repository
-	headPath = "/Users/iam-agf/github/shiken_server"
-	// Don't touch this value
-	problemsFolder = "problems"
-)
